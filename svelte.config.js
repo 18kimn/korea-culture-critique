72be08731d89be4ac79b8983a71a0d9e8dd39422
nathan kim <nathanckim18@gmail.com>
2023-02-28 22:51:43 -0500

    infra

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,30 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import resolveLinks from './src/hooks/resolveLinks.js';
+import addFootnotes from './src/hooks/addFootnotes.js';
+import makeTOC from './src/hooks/makeTOC.js';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			remarkPlugins: [resolveLinks, addFootnotes, makeTOC],
+			rehypePlugins: [
+				rehypeExternalLinks,
+				rehypeSlug,
+				rehypeAutolinkHeadings
+			]
+		})
+	],
+	extensions: ['.svelte', '.md'],
 	kit: {
 		adapter: adapter()
 	}
